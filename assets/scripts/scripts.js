5f940c8a17ede1a89b7c017e4c2958196e07507d
John Peart <john.peart@icloud.com>
2020-07-05 16:27:56 +0100

    Create scripts.js

---
+++ assets/scripts/scripts.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+  var menu = document.getElementById("header-menu-links");
+  menu.classList.toggle("open");
+  
+  var menu = document.getElementById("header-menu-toggle-image");
+  menu.classList.toggle("open");
+}
